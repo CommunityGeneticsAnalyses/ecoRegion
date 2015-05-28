ad7e0253fdab32c14fdbdf829f214619d56bbcac
MKLau <matthewklau@fas.harvard.edu>
2015-05-27 21:22:15 -0400

    update

--- src/global.R
+++ src/global.R
@@ -43,7 +43,7 @@ chPlot <- function(x,f,col,pch,se=FALSE,xlim=c(-1,1),ylim=c(-1,1)){
     bar.up <- mu + bars
     bar.lo <- mu - bars
 ### make the plot
-    plot(mu,col=col,pch=pch)
+    plot(mu,col=col,pch=pch,xlim=xlim,ylim=ylim)
     for (i in 1:nrow(mu)){
         lines(c(bar.up[i,1],bar.lo[i,1]),rep(mu[i,2],2),col=col[i])
         lines(rep(mu[i,1],2),c(bar.up[i,2],bar.lo[i,2]),col=col[i])
