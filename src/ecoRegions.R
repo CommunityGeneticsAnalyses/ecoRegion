59b0f94437137f4c352cbf1bba4ed0c83d9b7cd2
mklau <matthewklau@fas.harvard.edu>
2016-05-31 12:46:23 -0400

    update

--- src/ecoRegions.R
+++ src/ecoRegions.R
@@ -6,7 +6,7 @@ library(ggmap)
 stay <- read.csv('../data/AllLocations_EnvStay.csv')
 stay <- stay[,-8]
 stay$Time <- as.character(stay$Time)
-stay$Time[as.character(stay$Time) == "Current"] <- 'Current'
+stay$Time[as.character(stay$Time) == "Current"] <- '2010'
                                         #relativize climate variables
 stay[,5:7] <- apply(stay[,5:7],2,function(x) x/max(x))
                                         #ordinate
@@ -19,9 +19,11 @@ ord.col <- c('red','black','green')[as.numeric(stay$Ecoregion)]
 ord.time <- as.numeric(factor(stay$Time))
 ord.time <- c(1,0.45,0.5)[ord.time]
 ord.alpha <- apply(cbind(ord.col,ord.time),1,function(x) alpha(x[1],x[2]))
+
 ord.pch <- as.numeric(factor(stay$Time))
 ord.pch <- c(19,19,1)[ord.pch]
 f.stay <- paste(stay[,1],stay[,4])
+f.stay <- gsub('Current','2010',f.stay)
 leg.names <- unique(paste(stay[,1],stay[,4]))
 leg.names <- leg.names[c(3,1,2,6,4,5,9,7,8)]
 leg.names <- gsub('2010','1990',leg.names)
@@ -31,7 +33,7 @@ leg.col <- unique(ord.alpha)[c(3,1,2,6,4,5,9,7,8)]
 
 x <- move.all <- read.csv('../data/AllLocations_EnvMove.csv')
 move.all[,2:3] <- apply(move.all[,2:3],2,function(x) (x-mean(x))/sd(x))
-#move.all$Time <- gsub('Current','2010',move.all$Time)
+move.all$Time <- gsub('Current','2010',move.all$Time)
 vec.move <- envfit(move.all[,2:3],move.all[,5:7])
 move.col <- c('red','black','green')[as.numeric(move.all$Ecoregion)]
 move.time <- as.numeric(factor(move.all$Time))
@@ -39,7 +41,6 @@ move.time <- c(1,0.45,0.5)[move.time]
 move.alpha <- apply(cbind(move.col,move.time),1,function(x) alpha(x[1],x[2]))
 move.pch <- as.numeric(factor(move.all$Time))
 move.pch <- c(19,19,1)[move.pch]
-move.year <- 
 f <- paste(move.all[,1],move.all[,4])
 x.f <- paste(x[,1],x[,4])
 mu <- data.frame(apply(x[,2:3],2,function(x,f) tapply(x,f,mean),f=x.f))
@@ -73,21 +74,28 @@ dev.off()
 main <- readJPEG('../results/map.jpeg')
 inset <- readJPEG('../results/map_inset.jpeg')
 mu <- list(mu=apply(move.all[,2:3],2,function(x,f) tapply(x,f,mean),f=f))
+mu$mu <- mu$mu[c(3,1,2,6,4,5,9,7,8),]
 xlim <- c(-1.35,2.5);ylim <- c(-2.35,2)
 
 ### Fig 1. Ordination of ecoregions moving through climate space
 pdf('../results/EcoReg_FigA.pdf')
-sc <- chPlot(min.stay,f=f.stay,col=ord.alpha,pch=ord.pch,xlim=c(-1,1.25),ylim=c(-1,0.5),return.coord=TRUE,se=FALSE)
+sc <- chPlot(min.stay,f=f.stay,col=ord.alpha,pch=ord.pch,xlim=c(-1,1.25),ylim=c(-1,0.5),return.coord=TRUE,se=FALSE,cex=1.25)
+stay.lab <- rownames(sc[[1]][grepl('UHP',rownames(sc[[1]])),])
+stay.lab <- do.call(rbind,strsplit(stay.lab,' '))[,2]
+stay.lab <- gsub('2010','Current',stay.lab)
 plot(vec.stay,col=grey(0.75))
 text(c(-0.25,-0.25,0.65),c(-0.30,0.25,0.30),labels=c('CCV','SD','UHP'),col=c(2,1,3))
 chArrow(sc)
-text(sc[[1]][grepl('UHP',rownames(sc[[1]])),1:2],labels=substr(rownames(sc[[1]][grepl('UHP',rownames(sc[[1]])),]),5,8),pos=3,col='darkgrey')
+text(sc[[1]][grepl('UHP',rownames(sc[[1]])),1:2],labels=stay.lab,pos=3,col='darkgrey')
 #legend('bottomright',legend=leg.names,pch=rep(c(19,19,1),3),col=leg.col)
 dev.off()
 
 ### Fig 2. Plot of points moving through geographic space based on climate
 pdf('../results/EcoReg_FigB.pdf')
-sc.m <- chPlot(move.all[,2:3],f=f,col=move.alpha,pch=move.pch,xlim=xlim,ylim=ylim,cex=0.75,plot.axes=FALSE,xlab='Longitude (decimal)',ylab='Latitude (decimal)',return.coord=TRUE,se=FALSE)
+sc.m <- chPlot(move.all[,2:3],f=f,col=move.alpha,pch=move.pch,xlim=xlim,ylim=ylim,cex=1.25,plot.axes=FALSE,xlab='Longitude (decimal)',ylab='Latitude (decimal)',return.coord=TRUE,se=FALSE)
+move.lab <- rownames(sc.m[[1]][grepl('UHP',rownames(sc.m[[1]])),])
+move.lab <- do.call(rbind,strsplit(move.lab,' '))[,2]
+move.lab <- gsub('2010','Current',move.lab)
 plot(vec.move,col=grey(0.75))
 axis(side=1,at=seq(xlim[1],xlim[2],length=5),
      labels=round(seq(mean(x[,2])-(xlim[1]*sd(x[,2])),mean(x[,2])+(xlim[2]*sd(x[,2])),
@@ -98,7 +106,7 @@ axis(side=2,at=seq(ylim[1],ylim[2],length=5),
 text(c(-0.8997,1.35722411,-0.06612945),c(0.5084873,-0.7592822,1.0456123),
      labels=c('CCV','SD','UHP'),col=c(2,1,3))
 chArrow(mu)
-text(sc.m[[1]][grepl('UHP',rownames(sc.m[[1]])),1:2],labels=substr(rownames(sc.m[[1]][grepl('UHP',rownames(sc.m[[1]])),]),5,8),pos=3,col='darkgrey')
+text(sc.m[[1]][grepl('UHP',rownames(sc.m[[1]])),1:2],labels=move.lab,pos=3,col='darkgrey')
 rasterImage(main,0.75,0.15,2.5,2)
 dev.off()
 
